6fb225f980a47a0790040074d0c133496e7baa7d
whhong1993 <whhong1993@163.com>
2020-07-22 17:40:21 +0800

    refine

--- controller/TestController.php
+++ controller/TestController.php
@@ -32,6 +32,44 @@ class TestController extends ArkWebController
     public function server()
     {
         $this->app->server->serve()->send();
+        $message = $this->app->server->getMessage();
+        var_dump($message);die();
+    }
+
+    public function handleMessage()
+    {
+        $message = $this->app->server->getMessage();
+        var_dump($message);die();
+//        $this->app->server->getMessage(function ($message) {
+//            switch ($message->MsgType) {
+//                case 'event':
+//                    return '收到事件消息';
+//                    break;
+//                case 'text':
+//                    return '收到文字消息';
+//                    break;
+//                case 'image':
+//                    return '收到图片消息';
+//                    break;
+//                case 'voice':
+//                    return '收到语音消息';
+//                    break;
+//                case 'video':
+//                    return '收到视频消息';
+//                    break;
+//                case 'location':
+//                    return '收到坐标消息';
+//                    break;
+//                case 'link':
+//                    return '收到链接消息';
+//                    break;
+//                // ... 其它消息
+//                default:
+//                    return '收到其它消息';
+//                    break;
+//            }
+//
+//        });
     }
 
 
