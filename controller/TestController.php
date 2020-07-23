9d1f46d4c6634d869b065bccfc293e2f559b9c7e
whhong1993 <whhong1993@163.com>
2020-07-23 10:29:44 +0800

    add log

--- controller/TestController.php
+++ controller/TestController.php
@@ -8,7 +8,6 @@
 namespace wechat\controller;
 
 use EasyWeChat\Factory;
-use Exception;
 use sinri\ark\core\ArkLogger;
 use sinri\ark\web\implement\ArkWebController;
 use wechat\toolkit\Helper;
@@ -33,7 +32,7 @@ class TestController extends ArkWebController
 
     public function server()
     {
-        $logger = new ArkLogger(__DIR__ . '/log', __CLASS__);
+        $logger = new ArkLogger(__DIR__ . '/../log', __CLASS__);
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             $this->app->server->serve()->send();
         } else {
