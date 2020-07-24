fe0bb1fe40ff3ba76d1fd14aa1c904f710b74edc
whhong1993 <whhong1993@163.com>
2020-07-24 11:06:46 +0800

    add webhooks

---
+++ gitpull.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: whhong
+ * Date: 2020-07-24
+ * Time: 11:02
+ */
+
+ini_set('date.timezone', 'Asia/Shanghai');
+$target = '/var/www/wechat'; // 生产环境web目录
+//密钥
+$secret = "123456";
+//获取GitHub发送的内容
+$json = file_get_contents('php://input');
+$content = json_decode($json, true);
+//github发送过来的签名
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+if (!$signature) {
+    return http_response_code(404);
+}
+list($algo, $hash) = explode('=', $signature, 2);
+//计算签名
+$payloadHash = hash_hmac($algo, $json, $secret);
+// 判断签名是否匹配
+if ($hash === $payloadHash) {
+
+    $cmd = "cd $target && sudo git pull 2>&1";
+    $res = shell_exec($cmd);
+
+    $res_log = 'Success:' . PHP_EOL;
+    $res_log .= $cmd . '结果' . $res . PHP_EOL;
+    $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：' . PHP_EOL;
+    $res_log .= $res . PHP_EOL;
+    $res_log .= '=======================================================================' . PHP_EOL;
+    echo $res_log;
+} else {
+    $res_log = 'Error:' . PHP_EOL;
+    $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：' . PHP_EOL;
+    $res_log .= '密钥不正确不能pull' . PHP_EOL;
+    $res_log .= '=======================================================================' . PHP_EOL;
+    echo $res_log;
+}
