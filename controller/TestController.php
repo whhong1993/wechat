8d0014fc063bf22bd5635f290f3f2613f3f658e1
whhong1993 <whhong1993@163.com>
2020-07-24 15:15:29 +0800

    Auto reply

--- controller/TestController.php
+++ controller/TestController.php
@@ -41,42 +41,42 @@ class TestController extends ArkWebController
         } else {
             $message = $this->app->server->getMessage();
             $logger->info("接收到消息", $message);
-            $this->app->server->push($this->handleMessage($message));
+
+            $this->app->server->push(function ($message) {
+                switch ($message['MsgType']) {
+                    case 'event':
+                        return '收到事件消息';
+                        break;
+                    case 'text':
+                        return '收到文字消息';
+                        break;
+                    case 'image':
+                        return '收到图片消息';
+                        break;
+                    case 'voice':
+                        return '收到语音消息';
+                        break;
+                    case 'video':
+                        return '收到视频消息';
+                        break;
+                    case 'location':
+                        return '收到坐标消息';
+                        break;
+                    case 'link':
+                        return '收到链接消息';
+                        break;
+                    case 'file':
+                        return '收到文件消息';
+                    // ... 其它消息
+                    default:
+                        return '收到其它消息';
+                        break;
+                }
+            });
             $this->app->server->serve()->send();
         }
     }
 
-    public function handleMessage($message)
-    {
-        switch ($message->msgType) {
-            case 'event':
-                return '收到事件消息';
-                break;
-            case 'text':
-                return '收到文字消息';
-                break;
-            case 'image':
-                return '收到图片消息';
-                break;
-            case 'voice':
-                return '收到语音消息';
-                break;
-            case 'video':
-                return '收到视频消息';
-                break;
-            case 'location':
-                return '收到坐标消息';
-                break;
-            case 'link':
-                return '收到链接消息';
-                break;
-            case 'file':
-                return '收到文件消息';
-            // ... 其它消息
-            default:
-                return '收到其它消息';
-                break;
-        }
-    }
+
 
 }
